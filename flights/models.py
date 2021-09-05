6faeadf2f6e80c42164826bb978012f8e1187d3e
Venish P <sec19ec070@sairamtap.edu.in>
2021-09-06 01:02:47 +0530

    added migration and models

--- flights/models.py
+++ flights/models.py
@@ -5,3 +5,6 @@ class Flight(models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: from {self.origin} to {self.destination}for duration of {self.duration}"
